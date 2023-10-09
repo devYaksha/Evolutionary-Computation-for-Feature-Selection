9bebf9023cd1182e510909518d881b631d2063c6
devYaksha <gssantoz2012@gmail.com>
2023-10-09 13:35:58 -0300

    svm-predict

--- HRSVM-PY/PY-HRSVM.py
+++ HRSVM-PY/PY-HRSVM.py
@@ -1,21 +1,18 @@
-import ctypes
 import os
-from subprocess import call
-call(["ls", "-l"])
-
-"""
-Use of ctypes: 
-#gcc -fPIC -shared -o helloworld.so hello.c
-#g++ -fPIC -shared -o helloworld.so hello.cpp
-lib = ctypes.CDLL("HRSVM-PY/helloworld.so")
-
-"""
 
 def svmTrain(classifierKernel, trainDataBase, outputFolder):
     os.system(f'./HRSVM-PY/HRSVM/svm-train -k {classifierKernel} -a 1 "{trainDataBase}" "{outputFolder}"')
+    #svm-train <options> <training data> <model output folder>
+
+def svmPredict(dataPath, classifierPath, outputFolder):
+    """For predicting process, must provide testing data path to be predicted, classifier path, and prediction output path."""
+    os.system(f'sudo ./HRSVM-PY/HRSVM/svm-predict {dataPath} "{classifierPath}" "{outputFolder}"')
+    #svm-predict <testing data> <classifier> <prediction output>
 
 
+data = "/home/yksh/Desktop/GAs-for-Hierarchical-Classification/tests/test.dat"
+classifier = "/home/yksh/Desktop/GAs-for-Hierarchical-Classification/tests/output"
+predict = "/home/yksh/Desktop/GAs-for-Hierarchical-Classification/tests/predict/"
 
 
-
-svmTrain("binary", "/home/yksh/Desktop/GAs-for-Hierarchical-Classification/tests/train.dat", "/home/yksh/Desktop/GAs-for-Hierarchical-Classification/tests/output")
+svmPredict(data, classifier, predict)
---
+++ HRSVM-PY/ctypes/ctypes.py
@@ -0,0 +1,9 @@
+import ctypes
+
+"""
+Use of ctypes: 
+#gcc -fPIC -shared -o helloworld.so hello.c
+#g++ -fPIC -shared -o helloworld.so hello.cpp
+lib = ctypes.CDLL("HRSVM-PY/helloworld.so")
+
+"""
