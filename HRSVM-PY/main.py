961e96feea97de10337e93e6f3f6e1aa8657332e
devYaksha <gssantoz2012@gmail.com>
2023-10-13 20:09:02 -0300

    hrsvm changes

--- HRSVM-PY/main.py
+++ HRSVM-PY/main.py
@@ -1,9 +1,12 @@
 from hrsvmPy import *
+parameters = "-k hierarchical svmTest/my_hierarchy.h -a 1 -l 50 "
+trainFilePath = "svmTest/train.dat"
+trainModelOutput = "svmTest/trainModel"
+svmTrain(parameters, trainFilePath, trainModelOutput)
+
+
 
 data = "svmTest/test.dat"
 classifier = "svmTest/trainModel"
 predict = "svmTest/result"
-
-
-svmTrain(None, "svmTest/train.dat", "svmTest/trainModel")
 svmPredict(data, classifier, predict)
