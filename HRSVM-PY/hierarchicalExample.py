ca42b5787ecd51248476958df5fc4711c417eef4
devYaksha <gssantoz2012@gmail.com>
2023-10-13 20:52:56 -0300

    multi-label tests

--- HRSVM-PY/hierarchicalExample.py
+++ HRSVM-PY/hierarchicalExample.py
@@ -1,12 +1,14 @@
 from hrsvmPy import *
-parameters = "-k hierarchical HRSVM-PY\hierarchicalExample\train.hf -a 1 -l 50 "
-trainFilePath = "HRSVM-PY\hierarchicalExample\train.dat"
-trainModelOutput = "HRSVM-PY\hierarchicalExample\trainModel"
-svmTrain(parameters, trainFilePath, trainModelOutput)
 
 
+parameters = "-k hierarchical HRSVM-PY/hierarchicalExample/train.hf -a 1"
+trainFilePath = "HRSVM-PY/hierarchicalExample/train.dat"
+trainModelOutput = "HRSVM-PY/hierarchicalExample/trainModel"
+#svmTrain(parameters, trainFilePath, trainModelOutput)
 
-data = "svmTest/test.dat"
-classifier = "svmTest/trainModel"
-predict = "svmTest/result"
-svmPredict(data, classifier, predict)
+
+predictData = "HRSVM-PY/hierarchicalExample/test.dat"
+classifier = "HRSVM-PY/hierarchicalExample/trainModel"
+output = "HRSVM-PY/hierarchicalExample"
+
+svmPredict(predictData, classifier, output)
